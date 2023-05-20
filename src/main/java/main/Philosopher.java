db2794e5febae80d239a591ce8fabbfc172ae194
Wesley Zhao <tigerzhao999@gmail.com>
2023-05-20 12:22:25 -0700

    test not working

--- src/main/java/main/Philosopher.java
+++ src/main/java/main/Philosopher.java
@@ -2,8 +2,13 @@ package main;
 
 import java.util.Random;
 
+import static main.PhilosopherStatus.*;
+
 public class Philosopher implements Runnable {
-    private final int philosopherNumber;
+
+
+    private final int philosopherNumber; // same as ID
+    private final int NumberOfPhils = 5; // same as number of philosphers in the next line
     private static final PhilosopherStatus[] status = new PhilosopherStatus[5];
     public Philosopher(int philosopherNumber) {
         this.philosopherNumber = philosopherNumber;
@@ -31,4 +36,52 @@ public class Philosopher implements Runnable {
     private void thinking(int sleepTime) throws InterruptedException {
         Thread.sleep(sleepTime);
     }
+
+    /**
+     * returns the left neighbor
+     * @return the philosopherNumber of the left neighbor.
+     */
+    private int left_neighbor()
+    {
+        if (philosopherNumber == 0)
+        {
+            return NumberOfPhils - 1;
+        }
+        else
+        {
+            return (philosopherNumber - 1);
+        }
+    }
+    /**
+     * returns the right neighbor
+     * @return the philosopherNumber of the right neighbor.
+     */
+    private int right_neighbor()
+    {
+        if ((philosopherNumber + 1) == NumberOfPhils)
+        {
+            return 0;
+        }
+        else
+        {
+            return (philosopherNumber + 1);
+        }
+    }
+
+
+    private void test()
+    {
+        //if im hungry and left and right arent eatting then let me eat
+        if ((status[this.left_neighbor()] != EATING) && (status[philosopherNumber] == HUNGRY) && (status[this.right_neighbor()] != EATING))
+        {
+            status[philosopherNumber] = EATING;
+            // fIXX pthread_cond_signal(&cond_vars[id]);
+
+        }
+    }
+
+
+
+
+
 }
