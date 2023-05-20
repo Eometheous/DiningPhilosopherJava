2e7130a5be39a42eb3ac6fc6ea7d65a3f8d56ab5
Eometheous <jonathan.sthomas@icloud.com>
2023-05-20 12:13:09 -0700

    created array of philosophers and started running them via the executor service

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,24 @@
 package main;
 
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
+        Philosopher[] philosophers = new Philosopher[5];
+        for (int i = 0; i < 5; i++) {
+            philosophers[i] = new Philosopher(i);
+        }
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (Philosopher philosopher : philosophers) {
+            executorService.execute(philosopher);
+        }
+
+        executorService.shutdown();
+
         System.out.println("Hello world!");
     }
 }
