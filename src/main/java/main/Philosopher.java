66506f2f90e2bfbd5df458946184f16ef4115136
Eometheous <jonathan.sthomas@icloud.com>
2023-05-20 11:59:03 -0700

    added PhilosopherStatus enum to Philosopher added thinking method made philosopherNumber final

--- src/main/java/main/Philosopher.java
+++ src/main/java/main/Philosopher.java
@@ -3,9 +3,11 @@ package main;
 import java.util.Random;
 
 public class Philosopher implements Runnable {
-    private int philosopherNumber;
+    private final int philosopherNumber;
+    private PhilosopherStatus status;
     public Philosopher(int philosopherNumber) {
         this.philosopherNumber = philosopherNumber;
+        status = PhilosopherStatus.THINKING;
     }
     @Override
     public void run() {
@@ -15,7 +17,18 @@ public class Philosopher implements Runnable {
         while (timesThroughLoop < 5) {
             sleepTime = random.nextInt(5);
 
+            try {
+                System.out.printf("Philosopher %d is THINKING\n", philosopherNumber);
+                thinking(sleepTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
             timesThroughLoop++;
         }
+    }
+
+    private void thinking(int sleepTime) throws InterruptedException {
+        Thread.sleep(sleepTime);
     }
 }
