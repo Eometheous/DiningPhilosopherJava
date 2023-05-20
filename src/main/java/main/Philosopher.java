121dc7489369437b7f03d65d542ab2bcfd19b00f
Eometheous <jonathan.sthomas@icloud.com>
2023-05-20 11:50:56 -0700

    created constructor and started on run() method

--- src/main/java/main/Philosopher.java
+++ src/main/java/main/Philosopher.java
@@ -1,4 +1,21 @@
 package main;
 
-public class Philosopher {
+import java.util.Random;
+
+public class Philosopher implements Runnable {
+    private int philosopherNumber;
+    public Philosopher(int philosopherNumber) {
+        this.philosopherNumber = philosopherNumber;
+    }
+    @Override
+    public void run() {
+        int timesThroughLoop = 0;
+        int sleepTime;
+        Random random = new Random();
+        while (timesThroughLoop < 5) {
+            sleepTime = random.nextInt(5);
+
+            timesThroughLoop++;
+        }
+    }
 }
